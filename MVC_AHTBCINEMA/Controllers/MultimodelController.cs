c7f76b60d4778e6a1f0420656133a6953be356c5
nnguyen5968 <pinnguyen801@gmail.com>
2024-06-18 20:42:55 +0700

    [thanhtoan]

--- MVC_AHTBCINEMA/Controllers/MultimodelController.cs
+++ MVC_AHTBCINEMA/Controllers/MultimodelController.cs
@@ -1,11 +1,13 @@
 ﻿using AHTBCinema_NHOM4_SD18301.Models;
+using API_AHTBCINEMA.Models;
 using ASM_AHTBCINEMA_NHOM4_SD18301.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using MVC_AHTBCINEMA.Model;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using static Microsoft.AspNetCore.Razor.Language.TagHelperMetadata;
 
 namespace MVC_AHTBCINEMA.Controllers
 {
@@ -17,6 +19,7 @@ namespace MVC_AHTBCINEMA.Controllers
         {
             _context = context;
         }
+
         [HttpGet]
         public JsonResult LoadSeats(int id)
         {
@@ -29,11 +32,12 @@ namespace MVC_AHTBCINEMA.Controllers
             // Lấy danh sách tên ghế dựa trên các IdGhe
             var seatNames = _context.Ghes
                 .Where(g => gheIds.Contains(g.IdGhe))
-                .Select(g => g.TenGhe)
+                .Select(g => new { Id = g.IdGhe, Name = g.TenGhe }) // Chỉ lấy Id và Name của ghế
                 .ToList();
 
             return Json(seatNames);
         }
+
         public IActionResult Index()
         {
             var phimlist = _context.Phims.ToList();
@@ -69,7 +73,7 @@ namespace MVC_AHTBCINEMA.Controllers
 
             var suggestedMovies = _context.Phims
                 .Where(p => p.TheLoai == phim.TheLoai && p.IdPhim != phim.IdPhim)
-                .Take(4) // Limiting to 4 suggestions
+                .Take(4) // Giới hạn chỉ lấy 4 bộ phim gợi ý
                 .ToList();
 
             var viewModel = new Multimodel
@@ -81,6 +85,59 @@ namespace MVC_AHTBCINEMA.Controllers
             };
 
             return View(viewModel);
+        }
+        public IActionResult ThanhToan(string id, string idphim, int gioChieuId , string username)
+        {
+            // Tìm vé dựa trên id ghế
+            var ve = _context.Ves.FirstOrDefault(x => x.Ghe == id);
+            var phim = _context.Phims.FirstOrDefault(f => f.IdPhim == idphim);
+            // Lấy thông tin ca chiếu dựa trên gioChieuId
+            var cachieu = _context.GioChieus
+                .Include(c => c.CaChieus) // Đảm bảo load thông tin phim
+                .FirstOrDefault(x => x.IdGioChieu == gioChieuId);
+            var userid = _context.Users.Where(x=>x.Username == username).FirstOrDefault();
+
+            //GioChieu
+            var giochieu = _context.GioChieus.FirstOrDefault(n => n.IdGioChieu == gioChieuId);
+            var khuyenmai = 1;
+            var ngaykhuyenmai = cachieu.CaChieus.NgayChieu.DayOfWeek;
+            if(ngaykhuyenmai == DayOfWeek.Thursday || ngaykhuyenmai == DayOfWeek.Tuesday) 
+            {
+                khuyenmai = 1;
+            }
+            else
+            {
+                khuyenmai = 0;
+            }
+            var phantramkm = _context.KhuyenMais.FirstOrDefault(x=>x.IdKM == khuyenmai);
+            var phantram = 0;
+            if(phantramkm != null)
+            {
+                phantram = phantramkm.Phantram;
+            }
+            var taohoadon = new HoaDon
+            {
+                
+                IdVe = ve.IdVe,
+                Combo = "CB1",
+                NhanVien = "NV1",
+                KhachHang = userid.IdUser,
+                KhuyenMai = khuyenmai,
+                TongTien = ve.GiaVe - (ve.GiaVe * phantram/100),
+                TrangThai = "Đang chờ duyệt"
+            };
+           
+            // Chuẩn bị ViewModel để gửi tới view
+            var viewModel = new Multimodel
+            {
+                CaChieu = new List<CaChieu> { cachieu.CaChieus },
+                Ghe = _context.Ghes.Where(g => g.IdGhe == id).ToList(),
+                GioChieu = new List<GioChieu> {giochieu },
+                Ve = new List<Ve> { ve },
+                Phim = new List<Phim> { phim },
+            };
+
+            return View(viewModel); // Trả về view với viewModel đã tạo
         }
     }
 }
--- MVC_AHTBCINEMA/Model/Multimodel.cs
+++ MVC_AHTBCINEMA/Model/Multimodel.cs
@@ -1,4 +1,5 @@
 ﻿using AHTBCinema_NHOM4_SD18301.Models;
+using API_AHTBCINEMA.Models;
 using System.Collections.Generic;
 
 namespace MVC_AHTBCINEMA.Model
@@ -9,5 +10,9 @@ namespace MVC_AHTBCINEMA.Model
         public IEnumerable<Ghe> Ghe { get; set; }
         public IEnumerable<CaChieu> CaChieu { get; set; }
         public List<Phim> SuggestedMovies { get; internal set; }
+        public List<Ve> Ve { get; internal set; }
+        public List<GioChieu> GioChieu { get; internal set; }
+
+        public List<KhuyenMai> KhuyenMai { get; internal set; }
     }
 }
